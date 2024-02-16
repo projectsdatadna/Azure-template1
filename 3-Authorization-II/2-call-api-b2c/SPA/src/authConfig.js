1ece175e4388aafdd47c775fa5a8516296aafb77
karan <karan110102@gmail.com>
2024-02-16 17:20:59 +0530

    changed localhost to my web url

--- 3-Authorization-II/2-call-api-b2c/SPA/src/authConfig.js
+++ 3-Authorization-II/2-call-api-b2c/SPA/src/authConfig.js
@@ -82,7 +82,7 @@ export const msalConfig = {
  */
 export const protectedResources = {
     apiTodoList: {
-        endpoint: 'http://localhost:5000/api/todolist',
+        endpoint: 'http://thankful-plant-0897f3f0f.4.azurestaticapps.net/api/todolist',
         scopes: {
             read: ['https://karanorgdatadna.onmicrosoft.com/TodoList/ToDoList.Read'],
             write: ['https://karanorgdatadna.onmicrosoft.com/TodoList/ToDoList.ReadWrite'],
